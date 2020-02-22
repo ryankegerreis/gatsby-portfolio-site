a0c6aa5a616e33cdf92f97575f32abcddcd74e01
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-02-22 18:46:03 -0500

    removed package that breaks stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from 'react';
-import ReactTypingEffect from 'react-typing-effect';
+// import ReactTypingEffect from 'react-typing-effect';
 
 export default class Home extends Component {
-  render() {
 
+  render() {
     return (
       <Fragment>
         <section id='home-link'>
           <div id='home' className='flex-col'>
-            <h1><ReactTypingEffect staticText="Hi," text="I'm Ryan." eraseDelay={500000000} /></h1>
+            {/* <h1><ReactTypingEffect staticText="Hi," text="I'm Ryan." eraseDelay={500000000} /></h1> */}
             <p>I'm a web developer.</p>
           </div>
         </section>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
 
 import '../styles/index.css'
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
 
   return (
     <>
