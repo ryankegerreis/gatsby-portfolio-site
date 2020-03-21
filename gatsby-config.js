7c50718cd86ddb76e4e27f03c0bc988f0c903ddd
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-03-21 10:08:59 -0400

    Changed about info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Kegerreis | Portfolio`,
+    description: `Ryan Kegerreis is a Software Developer in Fort Lauderdale, Florida.`,
+    author: `Ryan Kegerreis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
