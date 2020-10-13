16b756c8a6277a4919c319e49e4f008074c2c5be
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-10-12 20:30:44 -0400

    Finished first page

--- src/components/Portfolio/PortfolioSection.jsx
+++ src/components/Portfolio/PortfolioSection.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 
+import Waves from '../../imageFiles/videos/Waves.mp4'
+
 export default class PortfolioSection extends Component {
   render() {
     return (
       <div className='Container'>
         <div className='Nav'>
-          <span>Portfolio</span>
-          <span>Blog</span>
+          <span>
+            <a className='Link' href='https://ryankegerreis.com/'>Portfolio</a>
+          </span>
+          <span>
+            <a className='Link' href='https://ryankegerreis.blog/'>Blog</a>
+          </span>
         </div>
-        <div className='Image'>
-          {/* <img src='../../images/gatsby-astronaut.png' /> */}
+        <video autoPlay muted loop src={Waves} className='BackgroundVid' />
+        <div className='GreetingContainer'>
+          <span className='Greeting'>Hi I'm Ryan, I build things on the internet.</span>
         </div>
-
       </div>
     )
   }
